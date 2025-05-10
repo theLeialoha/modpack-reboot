86bc7c3f803f6acb2bde404925b385a2c73eb9ff
Leialoha <github@leialoha.dev>
2025-05-10 13:01:06 -0500

    Grouping MMR recipes

--- kubejs/server_scripts/recipes/machines.js
+++ kubejs/server_scripts/recipes/machines.js
@@ -3,3 +3,11 @@
 ServerEvents.recipes((event) => {
   event.remove({ mod: "modular_machinery_reborn" });
 });
+
+RecipeViewerEvents.groupEntries("item", (event) => {
+  event.group(
+    /^modular_machinery_reborn:/,
+    "kubejs:modular_machinery_reborn",
+    "Modular Machinery Reborn",
+  );
+});
