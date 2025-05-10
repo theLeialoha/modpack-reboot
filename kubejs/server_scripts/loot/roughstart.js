cb4692ce3d1fb3ef1831780cc834a7149bfefdc3
Leialoha <github@leialoha.dev>
2025-05-10 12:13:09 -0500

    Fixed models

--- kubejs/server_scripts/loot/roughstart.js
+++ kubejs/server_scripts/loot/roughstart.js
@@ -1,13 +1,15 @@
 // priority: 0
 
 LootJS.modifiers((event) => {
-  /**
-   * First loot entry with a condition. Will drop if the player has fortune.
-   */
   const dropTwine = LootEntry.of("roughstart:twine").when((conditions) => {
     conditions.matchTool("farmersdelight:flint_knife");
     conditions.randomChance(0.25);
   });
 
+  const dropFlintShard = LootEntry.of("roughstart:flint_shard").when((conditions) => {
+    conditions.randomChance(0.25);
+  });
+
   event.addBlockModifier(["minecraft:grass", "minecraft:tall_grass"]).addSequenceLoot(dropTwine);
+  event.addBlockModifier(["minecraft:gravel"]).addSequenceLoot(dropFlintShard);
 });
--- kubejs/server_scripts/recipes/roughstart.js
+++ kubejs/server_scripts/recipes/roughstart.js
@@ -7,11 +7,16 @@ ServerEvents.recipes((event) => {
     S: "minecraft:stick",
   });
 
-  event.shaped("roughstart:flint_pickaxe", ["FFF", "TST", " S "], {
+  event.shaped("roughstart:flint_pickaxe", ["FFF", "TS ", " S "], {
     F: "minecraft:flint",
     T: "roughstart:twine",
     S: "minecraft:stick",
   });
 
   event.shapeless("minecraft:string", ["roughstart:twine", "roughstart:twine"]);
+  event.shapeless("minecraft:flint", [
+    "roughstart:flint_shard",
+    "roughstart:flint_shard",
+    "roughstart:flint_shard",
+  ]);
 });
