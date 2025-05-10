72e345aa329dd7a714237bd5c7c06b016b2f0c2c
Leialoha <github@leialoha.dev>
2025-05-09 20:53:01 -0500

    Fixed errors in scripts

--- kubejs/startup_scripts/coppertools.js
+++ kubejs/startup_scripts/coppertools.js
@@ -1,20 +1,18 @@
 // priority: 0
 
-ItemEvents.toolTierRegistry(event => {
-  event.add('copper', tier => {
-    tier.uses = 250
-    tier.speed = 4.0
-    tier.attackDamageBonus = 2.0
-    tier.level = 1
-    tier.enchantmentValue = 5
-    tier.repairIngredient = '#c:ingots/copper'
-  })
-})
+const copperTier = (tier) => {
+  tier.uses = 250
+  tier.speed = 4.0
+  tier.attackDamageBonus = 2.0
+  tier.level = 1
+  tier.enchantmentValue = 5
+  tier.repairIngredient = '#c:ingots/copper'
+};
 
 StartupEvents.registry('item', event => {
-  event.create('coppertools:copper_sword', 'sword').tier('copper');
-  event.create('coppertools:copper_pickaxe', 'pickaxe').tier('copper');
-  event.create('coppertools:copper_axe', 'axe').tier('copper');
-  event.create('coppertools:copper_shovel', 'shovel').tier('copper');
-  event.create('coppertools:copper_hoe', 'hoe').tier('copper');
+  event.create('coppertools:copper_sword', 'sword').modifyTier(copperTier);
+  event.create('coppertools:copper_pickaxe', 'pickaxe').modifyTier(copperTier);
+  event.create('coppertools:copper_axe', 'axe').modifyTier(copperTier);
+  event.create('coppertools:copper_shovel', 'shovel').modifyTier(copperTier);
+  event.create('coppertools:copper_hoe', 'hoe').modifyTier(copperTier);
 });
--- kubejs/startup_scripts/roughstart.js
+++ kubejs/startup_scripts/roughstart.js
@@ -1,42 +1,39 @@
 // priority: 0
 
-ItemEvents.toolTierRegistry(event => {
-  event.add('flint', tier => {
-    tier.uses = 20
-    tier.speed = 2.0
-    tier.attackDamageBonus = 1.0
-    tier.level = 0
-    tier.enchantmentValue = 15
-    tier.repairIngredient = 'minecraft:flint'
-  })
-});
+const flintTeir = (tier => {
+  tier.uses = 20
+  tier.speed = 2.0
+  tier.attackDamageBonus = 1.0
+  tier.level = 0
+  tier.enchantmentValue = 15
+  tier.repairIngredient = 'minecraft:flint'
+})
 
 StartupEvents.registry('item', event => {
-  event.create('roughstart:flint_sword', 'axe').tier('flint');
-  event.create('roughstart:flint_axe', 'axe').tier('flint');
-  event.create('roughstart:flint_pickaxe', 'pickaxe').tier('flint');
+  event.create('roughstart:flint_sword', 'axe').modifyTier(flintTeir);
+  event.create('roughstart:flint_axe', 'axe').modifyTier(flintTeir);
+  event.create('roughstart:flint_pickaxe', 'pickaxe').modifyTier(flintTeir);
   event.create('roughstart:twine');
 });
 
 BlockEvents.modification(event => {
 
   const woods = ["oak", "spruce", "birch", "jungle", "acacia", "dark_oak", "mangrove", "cherry"];
-
-  for (const wood of woods) {
+  woods.forEach(wood => {
     const types = ["crimson", "warped"].includes(wood) ? ["hyphae", "stem"] : ["log", "wood"];
 
-    for (const type of types) {
+    types.forEach(type => {
       event.modify('minecraft:' + wood + '_' + type, modify => {
         modify.requiresTool = true
       });
-
+    
       event.modify('minecraft:stripped_' + wood + '_' + type, modify => {
         modify.requiresTool = true
       });
-
+    
       event.modify('minecraft:' + wood + '_planks', modify => {
         modify.requiresTool = true
       });
-    }
-  }
+    });
+  });
 });
