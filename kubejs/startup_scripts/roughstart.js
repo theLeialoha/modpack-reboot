585f2762802d732dbcbbe4f35e05a85d6c78afdf
Leialoha <github@leialoha.dev>
2025-05-10 01:50:40 -0500

    Updated user scripts

--- kubejs/startup_scripts/roughstart.js
+++ kubejs/startup_scripts/roughstart.js
@@ -1,38 +1,37 @@
 // priority: 0
 
-const flintTeir = (tier => {
-  tier.uses = 20
-  tier.speed = 2.0
-  tier.attackDamageBonus = 1.0
-  tier.level = 0
-  tier.enchantmentValue = 15
-  tier.repairIngredient = 'minecraft:flint'
-})
+const flintTeir = (tier) => {
+  tier.uses = 20;
+  tier.speed = 2.0;
+  tier.attackDamageBonus = 1.0;
+  // tier.level = 0
+  tier.enchantmentValue = 15;
+  tier.repairIngredient = "minecraft:flint";
+};
 
-StartupEvents.registry('item', event => {
-  event.create('roughstart:flint_sword', 'axe').tier('wood').modifyTier(flintTeir);
-  event.create('roughstart:flint_axe', 'axe').tier('wood').modifyTier(flintTeir);
-  event.create('roughstart:flint_pickaxe', 'pickaxe').tier('wood').modifyTier(flintTeir);
-  event.create('roughstart:twine');
+StartupEvents.registry("item", (event) => {
+  event.create("roughstart:flint_sword", "axe").tier("wood").modifyTier(flintTeir);
+  event.create("roughstart:flint_axe", "axe").tier("wood").modifyTier(flintTeir);
+  event.create("roughstart:flint_pickaxe", "pickaxe").tier("wood").modifyTier(flintTeir);
+  event.create("roughstart:twine");
 });
 
-BlockEvents.modification(event => {
-
+BlockEvents.modification((event) => {
   const woods = ["oak", "spruce", "birch", "jungle", "acacia", "dark_oak", "mangrove", "cherry"];
-  woods.forEach(wood => {
+  woods.forEach((wood) => {
     const types = ["crimson", "warped"].includes(wood) ? ["hyphae", "stem"] : ["log", "wood"];
 
-    types.forEach(type => {
-      event.modify('minecraft:' + wood + '_' + type, modify => {
-        modify.requiresTool = true
+    types.forEach((type) => {
+      event.modify(`minecraft:${wood}_${type}`, (modify) => {
+        modify.requiresTool = true;
       });
-    
-      event.modify('minecraft:stripped_' + wood + '_' + type, modify => {
-        modify.requiresTool = true
+
+      event.modify(`minecraft:stripped_${wood}_${type}`, (modify) => {
+        modify.requiresTool = true;
       });
-    
-      event.modify('minecraft:' + wood + '_planks', modify => {
-        modify.requiresTool = true
+
+      event.modify(`minecraft:${wood}_planks`, (modify) => {
+        modify.requiresTool = true;
       });
     });
   });
