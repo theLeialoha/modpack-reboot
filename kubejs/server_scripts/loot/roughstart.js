88fcc43438f9e1ff529d8e796cfbb69a63fc759a
Leialoha <github@leialoha.dev>
2025-05-10 02:36:46 -0500

    Added more flavors

--- kubejs/server_scripts/loot/roughstart.js
+++ kubejs/server_scripts/loot/roughstart.js
@@ -1,16 +1,12 @@
 // priority: 0
 
-LootJS.modifiers(event => {
+LootJS.modifiers((event) => {
+  /**
+   * First loot entry with a condition. Will drop if the player has fortune.
+   */
+  const dropTwine = LootEntry.of("roughstart:twine").matchAllOf((conditions) => {
+    conditions.matchTool("roughstart:flint_sword").randomChance(0.25);
+  });
 
-
-    /**
-     * First loot entry with a condition. Will drop if the player has fortune.
-     */
-    const dropTwine = LootEntry.of("roughstart:twine")
-        .randomChance(0.5).when(c =>
-            c.matchTool(ItemFilter.item("roughstart:flint_sword"))
-        )
-
-    event.addBlockModifier([ "minecraft:grass", "minecraft:tall_grass" ])
-        .addSequenceLoot(dropTwine)
-})
+  event.addBlockModifier(["minecraft:grass", "minecraft:tall_grass"]).addSequenceLoot(dropTwine);
+});
